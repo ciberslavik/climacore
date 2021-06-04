e8db4d68f411827de49eda4435fec064653221f3
ciberslavik <ciberslavik@gmail.com>
2021-06-04 22:20:38 +0300

    11

---
+++ Tests/AgavaIOService.Test/AgavaIOModuleTests.cs
@@ -0,0 +1,23 @@
+using Clima.AgavaModBusIO.Model;
+using NUnit.Framework;
+
+namespace AgavaIOService.Test
+{
+    public class AgavaIoModuleTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CreateModule_Test()
+        {
+            var module = AgavaIoModule.CreateIoModule(
+                1,
+                new byte[] {0x03, 0x03, 0x06, 0x03, 0x03, 0x06});
+            
+            Assert.Pass();
+        }
+    }
+}
